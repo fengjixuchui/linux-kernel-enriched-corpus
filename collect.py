79957e9f41f610cbceafc843191ee4c0473bbd42
Palash Oswal <hello@oswalpalash.com>
2023-03-22 14:58:45 -0700

    Parallelize to make things faster

--- collect.py
+++ collect.py
@@ -4,32 +4,14 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import multiprocessing
 
 '''
 Query https://syzkaller.appspot.com/upstream/fixed for all bugs that have been fixed and have "C" and "syz" reproducers
 Save reproducers to text files
 '''
 
-def main():
-    # Query the page
-    bugs = []
-    page = requests.get("https://syzkaller.appspot.com/upstream/fixed")
-    soup = BeautifulSoup(page.content, 'html.parser')
-    # parse table rows
-    rows = soup.find_all('tr')
-    for row in rows:
-        # print row with class as "title" and first "stat"
-        title = row.find_all('td', class_="title")
-        stat = row.find_all('td', class_="stat")
-        # if title and stat exist
-        if title and stat:
-            # check if stat[0] contains "C" in "td"
-            if "C" in stat[0] or "syz" in stat[0]:
-                print(title[0].find('a').get('href'))
-                bugs.append(title[0].find('a').get('href'))
-
-    # for each bug, get the reproducers from "https://syzkaller.appspot.com/$bug"
-    for bug in bugs:
+def get_reproducers(bug):
         page = requests.get("https://syzkaller.appspot.com" + bug)
         soup = BeautifulSoup(page.content, 'html.parser')
         # parse last table in page that has class "list_table"
@@ -50,8 +32,28 @@ def main():
             with open("files/" + bug_id + "-" + x + ".txt", 'w+') as f:
                 f.write(page.text)
 
+def main():
+    # Query the page
+    bugs = []
+    page = requests.get("https://syzkaller.appspot.com/upstream/fixed")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # parse table rows
+    rows = soup.find_all('tr')
+    for row in rows:
+        # print row with class as "title" and first "stat"
+        title = row.find_all('td', class_="title")
+        stat = row.find_all('td', class_="stat")
+        # if title and stat exist
+        if title and stat:
+            # check if stat[0] contains "C" in "td"
+            if "C" in stat[0] or "syz" in stat[0]:
+                print(title[0].find('a').get('href'))
+                bugs.append(title[0].find('a').get('href'))
 
-
+    # for each bug, get the reproducers from "https://syzkaller.appspot.com/$bug"
+    # run the following code in 15 parallel processes to speed up
+    pool = multiprocessing.Pool(15)
+    pool.map(get_reproducers, bugs)
 
 if __name__ == "__main__":
     main()
